df4a2d44a5e8f362141746a6013d7a75baafbbe6
sancarder <sandra.derbring@gmail.com>
2020-11-30 23:56:36 +0100

    updated

--- 1/1.py
+++ 1/1.py
@@ -1,18 +1,17 @@
 
-def readData(data):
-    line = input.readline().strip()
-    return line.split(',') 
+def testData():
 
-def testData(test, answer):
+    otest = open('test.txt', 'r')
+    test = otest.readlines()
 
-    test = readData(open('test.txt', 'r'))
-    answer = readData(open('answer.txt', 'r'))
+    oanswer = open('answer.txt', 'r')    
+    answer = oanswer.readline()
 
     status = False
     print("Runs test data")
     result = runCode(test)
 
-    if result == answer:
+    if result == int(answer):
         status = True
  
     print(status)
@@ -22,14 +21,22 @@ def testData(test, answer):
 def runCode(data):
     print("Runs code")
     
-    return "default"
+    print(data)
+    
+    result = int(data[0].strip())*int(data[1].strip())
+    
+    print(result)
+    
+    return result
 
 #Runs testdata
 testResult = testData()
 
 if testResult == True:
     print("Test data parsed. Tries to run puzzle.")
-    puzzle = readData(open('input.txt', 'r'))
-    runCode(puzzle)
+    opuzzle = open('input.txt', 'r')
+    puzzle = opuzzle.readlines()
+    finalResult = runCode(puzzle)
+    print(finalResult)
 else:
     print("Test data failed. Code is not correct. Try again.")
